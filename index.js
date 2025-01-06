c50d6d2007cc0482ebf62d86cfc9a919824631f8
Sarvaiya Hiren <158996143+hiren4413@users.noreply.github.com>
2025-01-06 16:37:46 +0530

    Admin Panal

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+
+const port = 8000;
+
+const app = express();
+
+const db = require('./config/db');
+
+const path = require('path');
+
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
+
+app.set('view engine', 'ejs');
+
+const cookieParser = require('cookie-parser');
+app.use(cookieParser()) 
+
+// =================  passport Js Code  =================
+
+const passport = require('passport')
+const passportlocal = require('./config/passportlocal')
+const session = require('express-session')
+
+app.use(session({  
+    secret : 'admin',
+    resave : false, 
+    saveUninitialized : true,
+    cookie : { 
+        maxAge : 1000*60*60*24, 
+    }  
+}));   
+
+app.use(passport.initialize()); 
+app.use(passport.session());
+app.use(passport.setUser);     
+
+// =================  passport Js Code  =================
+
+app.use(express.urlencoded())
+app.use('/', express.static(path.join(__dirname, '/public')));      
+app.use('/',require('./routes/indexRoutes'));
+
+
+app.listen(port,(err)=>{  
+    if (err) {
+        console.log(err);
+        return false;
+    }
+    console.log(`server start on http://localhost:${port}/`);
+})
---
+++ routes/indexRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const routes = express.Router();
+
+routes.use('/',require('../routes/authRoute'));
+routes.use('/category', require('../routes/CategoryRoute'))
+routes.use('/subcategory', require('../routes/subCategoryRoute'))
+routes.use('/exsubcategory', require('../routes/exSubCategoryRoutes'))
+routes.use('/product', require('../routes/productRoutes'))
+
+module.exports = routes; 
